96a7a933837ef316fe1283bd8966321a1e32db39
Yesty-Anggela <Yestyanggela@gmail.com>
2025-07-27 22:32:30 +0700

    menambahkan modul ortu

---
+++ Models/ortu.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemAkademik.Models;
+
+public enum JenisKelamin
+{
+    Laki_Laki,
+    Perempuan
+}
+
+public class Siswa
+{
+    [Key]
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    [ForeignKey("User")]
+    public Guid UserId { get; set; }
+
+    [Required]
+    [StringLength(20)]
+    public string NISN { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string NamaLengkap { get; set; }
+
+    [Required]
+    [EnumDataType(typeof(JenisKelamin))]
+    public JenisKelamin JenisKelamin { get; set; }
+
+    public DateTime? TanggalLahir { get; set; }
+
+    public virtual User User { get; set; }
+}
